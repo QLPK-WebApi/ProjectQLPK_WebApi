472427a51f34b8bcf6bc5d53073eede9083442c7
HOAINHAN\Admin <phamhoainhan1989@gmail.com>
2026-08-20 19:41:51 +0700

    test commit

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -14,7 +14,7 @@ using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
 
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args);   
 
 // ------ Đăng ký vào DI cho Config Jwt ------
 builder.Services.Configure<JwtOption>(builder.Configuration.GetSection(JwtOption.SectionName));
